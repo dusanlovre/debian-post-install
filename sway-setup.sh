3dfb555cc16453bee6266c941cb8babcadc93405
Dušan Lovre <du.lovre@gmail.com>
2026-01-25 18:37:29 +0100

    sway

---
+++ sway-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Setting up sway"
+sudo apt update
+sudo apt install -y sway light swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome 
+
+if [ -d "$HOME/dotfiles" ]; then
+    echo "dotfiles directory already exists"
+else
+    git clone https://github.com/dusanlovre/dotfiles.git "$HOME/dotfiles"
+fi
+
+echo "Done with sway"
